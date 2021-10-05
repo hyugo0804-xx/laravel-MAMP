332eb3fb7e635ced25fa4cd89db5d902698bcde0
ゴリあん <kug.haha0804@gmail.com>
2021-10-05 14:49:44 +0900

    カテゴリー別絞り込み表示作成完了

--- Q-A/app/Http/Controllers/PostController.php
+++ Q-A/app/Http/Controllers/PostController.php
@@ -16,12 +16,26 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $q = \Request::query();
+
+        if(isset($q['category_id'])){
+
+        $posts = Post::latest()->where('category_id', $q['category_id'])->get();
         $posts->load('category','user');
 
         return view('posts.index',[
             'posts' => $posts,
         ]);
+
+        }else{
+            $posts = Post::latest()->get();
+        $posts->load('category','user');
+
+        return view('posts.index',[
+            'posts' => $posts,
+        ]);
+        }
+
     }
 
     /**
